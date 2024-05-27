976a4233b1c2dbeca46d96bce61cb5d405166f9c
FGAT3 <feliciengamelin@yahoo.fr>
2024-05-27 14:12:05 +0200

    exo finished

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-// First Labour : Heracles vs Nemean Lion
-// use your Figher class here
+const Fighter = require("./src/Fighter");
+
+const heracles = new Fighter("Heracles 🧔 ", 20, 6);
+const nemean = new Fighter("Nemean Lion 🦁 ", 11, 13);
+
+while (nemean.life > 0 && heracles.life > 0) {
+  console.log(heracles.fight(nemean));
+  console.log(nemean.fight(heracles));
+}
+
+// console.log(heracles);
+// console.log(nemean);
+// console.log(nemean.fight(heracles));
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,28 @@
-/* Fighter class definition */
+MAX_LIFE = 100; // est-ce que c'est une var ?
+
+class Fighter {
+  constructor(name, strength, dexterity, life) {
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+  }
+
+  fight(opponent) {
+    const damageInflicted = Math.max(
+      1,
+      Math.round(Math.random() * this.strength)
+    );
+    const damageAfterDefense = Math.max(
+      0,
+      damageInflicted - opponent.dexterity
+    );
+    const lifeAfterDamage = Math.max(0, opponent.life - damageAfterDefense);
+
+    opponent.life = lifeAfterDamage; // modifier la valeur de la vie à chaque itération
+
+    return `${this.name} inflict ${damageAfterDefense} at ${opponent.name}! ${opponent.name} has ${lifeAfterDamage} life points left`;
+  }
+}
+
+module.exports = Fighter;
